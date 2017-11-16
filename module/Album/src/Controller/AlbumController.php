64b60b4c392e551b96d25181af79994bc4b652ed
Ramon <Ramon@Ramons-MacBook-Pro.local>
2017-11-16 01:17:57 -0500

    add album

--- module/Album/src/Controller/AlbumController.php
+++ module/Album/src/Controller/AlbumController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Album\Controller;
 
+use Album\Form\AlbumForm;
+use Album\Model\Album;
 use Album\Model\AlbumTable;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
@@ -20,6 +22,27 @@ class AlbumController extends AbstractActionController {
 	}
 
 	public function addAction() {
+		$form = new AlbumForm();
+		$form->get('submit')->setValue('Add');
+
+		$request = $this->getRequest();
+
+		if (!$request->isPost()) {
+			return ['form' => $form];
+		}
+
+		$album = new Album();
+		$form->setInputFilter($album->getInputFilter());
+		$form->setData($request->getPost());
+
+		if (!$form->isValid()) {
+			return ['form' => $form];
+		}
+
+		$album->exchangeArray($form->getData());
+		$this->table->saveAlbum($album);
+		return $this->redirect()->toRoute('album');
+
 	}
 
 	public function editAction() {
